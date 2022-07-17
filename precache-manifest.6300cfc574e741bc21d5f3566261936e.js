52928577eefe37fe61899720003e20a6d23034a5
Daniel Mason <danmasson0@gmail.com>
2022-07-17 17:59:35 -0400

    Deploy React app to GitHub Pages

---
+++ precache-manifest.6300cfc574e741bc21d5f3566261936e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5545d64d11e353c0c54ce75bec1ac13f",
+    "url": "/danielmason89/tours-react-use-effect-practise.git/index.html"
+  },
+  {
+    "revision": "e4d67487503f731b18ea",
+    "url": "/danielmason89/tours-react-use-effect-practise.git/static/css/main.8e49c9d7.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/danielmason89/tours-react-use-effect-practise.git/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/danielmason89/tours-react-use-effect-practise.git/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4d67487503f731b18ea",
+    "url": "/danielmason89/tours-react-use-effect-practise.git/static/js/main.7e51e064.chunk.js"
+  },
+  {
+    "revision": "a7d9f39071c80e5ed0c4",
+    "url": "/danielmason89/tours-react-use-effect-practise.git/static/js/runtime-main.c289105c.js"
+  }
+]);
